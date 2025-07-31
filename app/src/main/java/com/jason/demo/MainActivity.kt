93030ae972866c311d96c792bb0c201c8b50b4cd
Jason <myandroid0825@gmail.com>
2025-07-31 14:37:01 +0800

    run

--- app/src/main/java/com/jason/demo/MainActivity.kt
+++ app/src/main/java/com/jason/demo/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.jason.demo
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.app_bar_layout).setOnClickListener {
+            startActivity(Intent(this@MainActivity, AppLayoutActivity::class.java))
+        }
     }
 }
