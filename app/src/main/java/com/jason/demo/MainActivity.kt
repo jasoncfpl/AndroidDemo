09632b40fd954db2bb2190cab46f79a9aabc92ec
Jason <myandroid0825@gmail.com>
2025-07-31 16:00:40 +0800

    修改布局

--- app/src/main/java/com/jason/demo/MainActivity.kt
+++ app/src/main/java/com/jason/demo/MainActivity.kt
@@ -2,14 +2,18 @@ package com.jason.demo
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.app_bar_layout).setOnClickListener {
